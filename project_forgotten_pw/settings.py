f6d668ecc1d21bc624cff905647f59f9e2a6935e
Finn <finnmortenthen@gmail.com>
2020-09-26 17:55:49 +0200

    second commit

--- project_forgotten_pw/settings.py
+++ project_forgotten_pw/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3&3i@$wg%976ysiy)u04*u6ebnsn(-uzv66phfv1$k07)$=68!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
